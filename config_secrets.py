28f014abb3f85e4f896434e8d904fc62da96ec0a
Bn4 <bnfour@bnfour.net>
2023-03-23 16:27:55 +0700

    config default value improvements
    - less edgy placeholder address (still won't work out of the box)
    - all bots are disabled by default
    - admin list is empty now, so whoever is tg user id 123 is no longer an admin by default

--- config_secrets.py
+++ config_secrets.py
@@ -1,9 +1,9 @@
 # without the protocol (https://), also keep in mind /{token} will be added to this
-SERVER: str = "localhost:31337/whatever"
+SERVER: str = "localhost:8081"
 
 # tokens
-LADDER_BOT_TOKEN: str | None = "something"
-CAT_MACRO_BOT_TOKEN: str | None = "something else"
+LADDER_BOT_TOKEN: str | None = None
+CAT_MACRO_BOT_TOKEN: str | None = None
 
 # numeric account ids, not usernames
-ADMINS: tuple[int] = (123, )
+ADMINS: tuple[int] = tuple() # like (123, 234)
