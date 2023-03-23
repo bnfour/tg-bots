08abab5c083fa620ad638bbdde2a8f7e930a8035
Bn4 <bnfour@bnfour.net>
2023-03-23 16:09:59 +0700

    typing for configuration
    with a hint of an upcoming feature as well

--- config_secrets.py
+++ config_secrets.py
@@ -1,11 +1,9 @@
-# super seecret stuff
-
 # without the protocol (https://), also keep in mind /{token} will be added to this
-SERVER = "localhost:31337/whatever"
+SERVER: str = "localhost:31337/whatever"
 
 # tokens
-LADDER_BOT_TOKEN = "something"
-CAT_MACRO_BOT_TOKEN = "something else"
+LADDER_BOT_TOKEN: str | None = "something"
+CAT_MACRO_BOT_TOKEN: str | None = "something else"
 
 # numeric account ids, not usernames
-ADMINS = (123, )
+ADMINS: tuple[int] = (123, )
