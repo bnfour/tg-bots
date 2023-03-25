8719b2e19fbbc69a0412e8ea18c1b7f58c42550c
Bn4 <bnfour@bnfour.net>
2023-03-25 15:22:43 +0700

    all bot-related code moved to BotManager

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 # main app file, simple bottle app
 from bottle import route, run, request, static_file, template, HTTPResponse
+from bot_manager import BotManager
 
-from ladder_bot import LadderBot
-from cat_macro_bot import CatMacroBot
-
-from bot_info import BotInfo
-
-# TODO move bots to a factory/manager class, and use secrets from there
-import config_secrets as cfg
-
-# instances to be used
-bots = (LadderBot(cfg.SERVER, cfg.LADDER_BOT_TOKEN, cfg.ADMINS),
-    CatMacroBot(cfg.SERVER, cfg.LADDER_BOT_TOKEN, cfg.ADMINS))
-# their info, and an empty value to generate a placeholder
-bots_info: tuple[BotInfo] = tuple([bot.get_data() for bot in bots] + [BotInfo.get_placeholder_info(),])
-
+manager = BotManager()
 # 8080 was already taken on my host
 PORT = 8081
 
@@ -22,17 +10,13 @@ PORT = 8081
 # returns static page with basic info
 @route("/")
 def return_index_page() -> HTTPResponse:
-    return template("views/index.tpl", data=bots_info)
+    return template("views/index.tpl", data=manager.get_info())
 
 
 # handles any post request, if url matches bot's token, it's getting called
 @route("/<string>", method="POST")
 def handle_webhook(string: str) -> HTTPResponse:
-    for bot in bots:
-        if bot.is_active() and bot.is_token(string):
-            return bot.handle_update(request.json)
-    else:
-        return HTTPResponse(status=404)
+    return manager.handle_request(string, request.json)
 
 
 # handles requests to pictures
---
+++ bot_manager.py
@@ -0,0 +1,34 @@
+from bottle import HTTPResponse
+
+import config_secrets
+
+from bot_info import BotInfo
+from ladder_bot import LadderBot
+from cat_macro_bot import CatMacroBot
+
+class BotManager(object):
+    "Incapsulates all interaction with BotBase descendants."
+
+    def __init__(self):
+        self._bots = (
+            LadderBot(config_secrets.SERVER, config_secrets.LADDER_BOT_TOKEN, config_secrets.ADMINS),
+            CatMacroBot(config_secrets.SERVER, config_secrets.CAT_MACRO_BOT_TOKEN, config_secrets.ADMINS)
+        )
+        # actual bot info, and an empty value to generate a placeholder
+        self._info: tuple[BotInfo] = tuple([bot.get_data() for bot in self._bots] + [BotInfo.get_placeholder_info(),])
+
+    def get_info(self) -> tuple[BotInfo]:
+        "Returns info about configured bots, and a placeholder value"
+        # TODO wait, why it returns the placeholder here?
+        return self._info
+
+    def handle_request(self, token: str, body: str) -> HTTPResponse:
+        "Calls a bot by its token, or returns 404 for unknown value"
+        for bot in self._bots:
+            if bot.is_active() and bot.is_token(token):
+                try:
+                    return bot.handle_update(body)
+                except:
+                    return HTTPResponse(status=500)
+        else:
+            return HTTPResponse(status=404)
